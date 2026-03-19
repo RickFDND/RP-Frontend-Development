0e79a4a86f7cf190d69ebb35d072131ec1fc6f55
RickFDND <rick.pijnacker@hva.nl>
2026-03-19 17:26:11 +0100

    install sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import favicon from "$lib/assets/favicon.svg";
+	import global from "global.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<header>
+  <img src="/logo.png" alt="" width="150" />
+</header>
+
+<style>
+  header {
+    display: flex;
+    align-items: center;
+    height: 80px;
+  }
+</style>
